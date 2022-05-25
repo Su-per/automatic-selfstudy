d4a6b5d2a2fff06b680a6c9025d9e0f3edf904c1
이주원 <60950583+Su-per@users.noreply.github.com>
2022-05-25 09:40:02 +0900

    Update background_apply.py
    안마의자 신청 추가

--- background_apply.py
+++ background_apply.py
@@ -1,7 +1,7 @@
 from threading import Thread
 from datetime import datetime
 from random import randint
-from config import header, APPLY_URL
+from config import header, SELFSTUDY_APPLY_URL, MASSAGE_APPLY_URL
 import aiohttp
 import asyncio
 import json
@@ -9,36 +9,50 @@ import json
 
 class BackgroundApply(Thread):
     db = []
-    hour = 0
-    minute = 0
+    selfstudy_time = (0, 0)
+    massage_time = (0, 0)
 
-    def __init__(self, db, hour, minute):
+    def __init__(self, db, selfstudy_time, massage_time):
         self.db = db
-        self.hour = hour
-        self.minute = minute
+        self.selfstudy_time = selfstudy_time
+        self.massage_time = massage_time
         super().__init__()
 
-    async def request_apply(self, dict):
+    async def request_apply(self, dict, apply_type):
         await asyncio.sleep(0.1 * randint(1, 10))
+
+        if apply_type == "selfstudy":
+            url = SELFSTUDY_APPLY_URL
+        elif apply_type == "massage":
+            url = MASSAGE_APPLY_URL
+
         for _ in range(10):
             async with aiohttp.ClientSession() as session:
                 header["authorization"] = dict["access_token"]
-                async with session.put(APPLY_URL, headers=header) as response:
+                async with session.put(url=url, headers=header) as response:
                     res = json.loads(await response.text())
                     if "success" in res:
                         print(f"{dict['email']} 성공")
                         return
         print(f"{dict['email']} 실패")
+        print(res)
 
-    def apply(self):
-        if len(self.db) == 0:
+    def apply(self, apply_type):
+        if len([i for i in self.db if i["apply_type"] == apply_type]) == 0:
             return
-        tasks = [self.request_apply(i) for i in self.db]
+        tasks = [
+            self.request_apply(i, apply_type)
+            for i in [i for i in self.db if i["apply_type"] == apply_type]
+        ]
         asyncio.run(asyncio.wait(tasks))
 
     def run(self):
+        do_selfstudy, do_massage = False, False
         while True:
             dt = datetime.now()
-            if dt.hour == self.hour and dt.minute == self.minute:
-                self.apply()
-                break
+            if (dt.hour, dt.minute) == self.selfstudy_time and do_selfstudy == False:
+                self.apply(apply_type="selfstudy")
+                do_selfstudy = True
+            elif (dt.hour, dt.minute) == self.massage_time and do_massage == False:
+                self.apply(apply_type="massage")
+                do_massage = True
