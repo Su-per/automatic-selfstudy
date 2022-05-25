7c5326b68c4a357059fb98c3984ec0b14cdf3996
이주원 <60950583+Su-per@users.noreply.github.com>
2022-05-25 09:40:19 +0900

    Update config.py
    안마의자 신청 추가

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 SIGNIN_URL = "https://server.dotori-gsm.com/v1/signin"
-APPLY_URL = "https://server.dotori-gsm.com/v1/member/selfstudy"
+SELFSTUDY_APPLY_URL = "https://server.dotori-gsm.com/v1/member/selfstudy"
+MASSAGE_APPLY_URL = "https://server.dotori-gsm.com/v1/member/massage"
 
 header = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
