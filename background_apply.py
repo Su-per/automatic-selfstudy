5663ed3d73619f6fdcdf3db6853732987ecfa8eb
이주원 <60950583+Su-per@users.noreply.github.com>
2022-05-26 17:17:02 +0900

    Update background_apply.py

--- background_apply.py
+++ background_apply.py
@@ -9,16 +9,30 @@ import json
 
 class BackgroundApply(Thread):
     db = []
-    time = (0, 0)
+    apply_time = (0, 0)
 
-    def __init__(self, db, time, apply_type):
+    def __init__(self, db, apply_time, apply_type):
         self.db = db
-        self.time = time
+        self.apply_time = apply_time
         self.apply_type = apply_type
         super().__init__()
 
     async def request_apply(self, dict):
-        pass
+
+        if self.apply_type == "selfstudy":
+            url = SELFSTUDY_APPLY_URL
+        elif self.apply_type == "massage":
+            url = MASSAGE_APPLY_URL
+
+        for _ in range(100):
+            async with aiohttp.ClientSession() as session:
+                header["authorization"] = dict["access_token"]
+                async with session.put(url=url, headers=header) as response:
+                    res = json.loads(await response.text())
+                    if "success" in res:
+                        print(f"{dict['email']} 성공")
+                        return
+        print(f"{dict['email']} 실패")
 
     def apply(self):
         if len([i for i in self.db if i["apply_type"] == self.apply_type]) == 0:
@@ -32,6 +46,6 @@ class BackgroundApply(Thread):
     def run(self):
         while True:
             dt = datetime.now()
-            if (dt.hour, dt.minute) == self.time:
+            if (dt.hour, dt.minute) == self.apply_time:
                 self.apply()
                 break
