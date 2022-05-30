e27d3cbd01d210243c9f865dc81524a207e2049c
이주원 <60950583+Su-per@users.noreply.github.com>
2022-05-30 12:21:49 +0900

    Update background_apply.py

--- background_apply.py
+++ background_apply.py
@@ -18,17 +18,7 @@ class BackgroundApply(Thread):
         super().__init__()
 
     async def request_apply(self, dict):
-        url = APPLY_URL[self.apply_type]
-        header["authorization"] = dict["access_token"]
-
-        for _ in range(10):
-            async with aiohttp.ClientSession() as session:
-                async with session.put(url=url, headers=header) as response:
-                    res = json.loads(await response.text())
-                    if "success" in res:
-                        print(f"{dict['email']} 성공")
-                        return
-        print(f"{dict['email']} 실패", res["timeStamp"])
+        pass
 
     def apply(self):
         apply_db = [i for i in self.db if i["apply_type"] == self.apply_type]
